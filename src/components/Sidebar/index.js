26fe08e52f4e0a7beb4b26954af53c95d0bcbbbd
Jonas <jonaslagoteixeira@gmail.com>
2020-02-04 22:29:50 -0300

    estilizado componente sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React from 'react'
 import * as S from './styles'
+import Profile from '../Profile'
 
 const Sidebar = () => (
-  
+  <S.SidebarWrapper>
+    <Profile />
+  </S.SidebarWrapper>
 )
 
 export default Sidebar
