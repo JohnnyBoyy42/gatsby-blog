6b8e3041f330a693dcfd846029a48ec1ca2fbf89
Jonas <jonaslagoteixeira@gmail.com>
2020-02-04 22:17:58 -0300

    feito estilizacao do componente profile

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+
   html, body, div, span, applet, object, iframe,
   h1, h2, h3, h4, h5, h6, p, blockquote, pre,
   a, abbr, acronym, address, big, cite, code,
@@ -30,7 +32,7 @@ const GlobalStyles = createGlobalStyle`
     line-height: 1;
     background: #16202c;
     font-size: 100%;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif,
+    font-family: 'Roboto', sans-serif
   }
   ol, ul {
     list-style: none;
