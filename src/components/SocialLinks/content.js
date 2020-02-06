34e3521155df0e9376f476ed00a4fbe8bb51ddaa
Jonas <jonaslagoteixeira@gmail.com>
2020-02-06 14:52:41 -0300

    Feito os links dos icones

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const Links = [
+  {
+    label: 'Github',
+    url: 'https://https://github.com/JohnnyBoyy42',
+  },
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/JohnnyyBoyy42',
+  },
+  {
+    label: 'Youtube',
+    url: 'https://www.youtube.com/',
+  },
+  {
+    label: 'Instagram',
+    url: 'https://www.instagram.com/jonaass42/',
+  },
+  {
+    label: 'Unsplash',
+    url: 'https://unsplash.com/@willianjusten',
+  },
+]
+
+export default Links
