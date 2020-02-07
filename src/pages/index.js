5ee3c1a0fcc3b836d4a207c3e8649d653cd1d4a4
Jonas <jonaslagoteixeira@gmail.com>
2020-02-06 22:04:26 -0300

    feita configuracao do tranformer remark

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,15 @@ import PostItem from '../components/PostItem'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <PostItem />
+    <PostItem
+      slug="/about/"
+      background="#7159c1"
+      category="Misc"
+      date="06 de Fevereiro de 2020"
+      timeToRead="5"
+      title="Artigo Teste"
+      description="ola sasjaisa lasoasns ashuasjopas asuhsa sauh h asuhas asuihasosaas"
+    />
   </Layout>
 )
 
