67485edaab3e8cc99d8d3f2bdda1d3bc34899606
Jonas <jonaslagoteixeira@gmail.com>
2020-02-08 13:59:31 -0300

    feito a estrutura do componenete pagination

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,5 +1,35 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
 
-const Pagination = () => {}
+import * as S from './styles'
+
+const Pagination = ({
+  isFirst,
+  isLast,
+  currentPage,
+  numPages,
+  prevPage,
+  nextPage,
+}) => (
+  <S.PaginationWrapper>
+    {!isFirst && <Link to={prevPage}>← página anterior</Link>}
+
+    <p>
+      {currentPage} de {numPages}
+    </p>
+
+    {!isLast && <Link to={nextPage}>proxima página →</Link>}
+  </S.PaginationWrapper>
+)
+
+Pagination.propTypes = {
+  isFirst: PropTypes.bool.isRequired,
+  isLast: PropTypes.bool.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  numPages: PropTypes.number.isRequired,
+  prevPage: PropTypes.string,
+  nextPage: PropTypes.string,
+}
 
 export default Pagination
