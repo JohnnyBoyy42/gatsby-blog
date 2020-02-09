1e2a6d80909efed2bae1a76a5f2a958806a674d8
Jonas <jonaslagoteixeira@gmail.com>
2020-02-08 22:06:07 -0300

    feito configuracao do algolia

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config()
+
+const queries = require('./src/utils/algolia_queries')
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Blog`,
@@ -48,12 +52,23 @@ module.exports = {
             },
           },
           'gatsby-remark-lazy-load',
-          'gatsby-remark-prismjs',
+          'gatsby-remark-prismjs', //tem q ser o ultimo
         ],
       },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-algolia-search`,
+      options: {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        indexName: process.env.ALGOLIA_INDEX_NAME, // for all queries
+        queries,
+        chunkSize: 10000,
+        enablePartialUpdates: true,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
