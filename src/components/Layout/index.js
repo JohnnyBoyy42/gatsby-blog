6d32190bd21d94701db7977dfdbe35c360855325
Jonas <jonaslagoteixeira@gmail.com>
2020-02-10 11:12:37 -0300

    feito sidebar e menubar fixos durante animacao

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Sidebar from '../Sidebar'
 import MenuBar from '../MenuBar'
 import GlobalStyles from '../../styles/global'
+import { TransitionPortal } from 'gatsby-plugin-transition-link'
 
 import * as S from './styles'
 
@@ -10,9 +11,13 @@ const Layout = ({ children }) => {
   return (
     <S.LayoutWrapper>
       <GlobalStyles />
-      <Sidebar />
+      <TransitionPortal level="top">
+        <Sidebar />
+      </TransitionPortal>
       <S.LayoutMain>{children}</S.LayoutMain>
-      <MenuBar />
+      <TransitionPortal level="top">
+        <MenuBar />
+      </TransitionPortal>
     </S.LayoutWrapper>
   )
 }
